75c6745328632117b8fdbdbc2489d3805e8adeda
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-24 21:28:02 -0300

    cadastro de entretenimento

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,25 +7,28 @@
       <div class="col-md-10">
         <div class="container">
           <h3> <i class="fa fa-th-list"></i> Entretenimentos </h3>
-          <div v-if="cursos.length > 0">
+          <div v-if="entretenimentos.length > 0">
+            <p v-if="message.length > 0" class="alert alert-success"> {{message}} </p>
             <table class="table table-striped">
               <thead class="thead">
                 <tr>
                   <th scope="col"> Nome </th>
-                  <th> Ação </th>
+                  <th scope="col"> CNPJ </th>
+                  <th scope="col"> Endereco </th>
+                  <th scope="col"> Categoria </th>
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="curso of cursos" :key="curso.nome">
-                  <td> {{curso.nome}} </td>
-                  <td>
-                    <i class="fa fa-eye"></i> <span> <router-link to=""> ver entretenimento </router-link> </span>
-                  </td>
+                <tr v-for="entretenimento of entretenimentos" :key="entretenimento.nome">
+                  <td> {{entretenimento.nome}} </td>
+                  <td> {{entretenimento.cnpj}} </td>
+                  <td> {{entretenimento.endereco}} </td>
+                  <td> {{entretenimento.categoria}} </td>
                 </tr>
               </tbody>
             </table>
           </div>
-          <div v-if="!cursos.length > 0">
+          <div v-if="!entretenimentos.length > 0">
             <h3> Nenhum curso cadastrado! </h3>
             <p> Por favor, cadastre um novo Entretenimentos </p>
             <router-link to="/novo-entretenimento"> Novo Entretenimentos </router-link>
@@ -46,7 +49,27 @@ export default {
   },
   data: function () {
     return {
-      cursos: {}
+      url: 'http://localhost:4000/entretenimentos',
+      entretenimentos: {},
+      message: ''
+    }
+  },
+
+  created: function () {
+    console.log(this.$route.params)
+    if (typeof this.$route.params.cadastro !== 'undefined') {
+      this.message = 'Cadastro efetuado com sucesso!'
+    }
+
+    this.getEntretenimentos()
+  },
+
+  methods: {
+    getEntretenimentos: function () {
+      const self = this
+      self.$http.get(self.url)
+        .then(res => { self.entretenimentos = res.data })
+        .catch(err => console.log(err))
     }
   }
 }
--- src/views/NovoEntretenimento.vue
+++ src/views/NovoEntretenimento.vue
@@ -7,23 +7,30 @@
         <div class="col-md-10">
           <h3> <i class="fa fa-pencil"></i> Novo Entretenimento </h3>
           <p>Cadastre os dados para um novo entretenimento</p>
-          <form action="?" method="post">
+          <p v-if="error.length > 0" class="alert alert-danger"> {{error}} </p>
+          <form v-on:submit.prevent action="?" method="post">
             <div class="form-group">
-              <input type="text" name="nome" class="form-control" placeholder="Nome do Entretenimento">
+              <input v-model="nome" type="text" name="nome" class="form-control" placeholder="Nome do Entretenimento">
             </div>
             <div class="form-group">
-              <input type="text" name="cnpj" class="form-control" placeholder="CNPJ">
+              <input v-model="cnpj" type="text" name="cnpj" class="form-control" placeholder="CNPJ">
             </div>
             <div class="form-group">
-              <input type="text" name="endereco" class="form-control" placeholder="Endereço">
+              <input v-model="endereco" type="text" name="endereco" class="form-control" placeholder="Endereço">
             </div>
             <div class="form-group">
-              <select name="categoria" class="form-control">
+              <select v-model="categoria" name="categoria" class="form-control">
                   <option value=""> Selecione uma categoria </option>
+                  <option value="Diversão"> Diversão </option>
+                  <option value="Musica"> Musica </option>
+                  <option value="Filmes"> Filmes </option>
+                  <option value="Tecnologia"> Tecnologia </option>
+                  <option value="Trip"> Trip </option>
+                  <option value="Educacional"> Educacional </option>
               </select>
             </div>
             <div class="form-group">
-              <button class="btn btn-primary to-right"> <i class="fa fa-plus"></i> Novo Entretenimento</button>
+              <button @click="cadastro" class="btn btn-primary to-right"> <i class="fa fa-plus"></i> Novo Entretenimento</button>
             </div>
           </form>
       </div>
@@ -31,6 +38,7 @@
   </section>
   </div>
 </template>
+
 <script>
 import MenuTopo from '@/components/MenuTopo.vue'
 import Menu from '@/components/Menu.vue'
@@ -41,7 +49,34 @@ export default {
   },
   data: function () {
     return {
-      cursos: {}
+      url: 'http://localhost:4000/entretenimento/new',
+      nome: '',
+      cnpj: '',
+      endereco: '',
+      categoria: '',
+      error: ''
+    }
+  },
+  methods: {
+    cadastro: function () {
+      const self = this
+
+      if (self.nome === '' || self.cnpj === '' || self.endereco === '' || self.categoria === '') {
+        self.error = 'Por favor, informe todos os dados!'
+        return false
+      }
+
+      self.$http.post(self.url, {
+        nome: self.nome,
+        cnpj: self.cnpj,
+        endereco: self.endereco,
+        categoria: self.categoria
+      })
+        .then(res => self.redirect())
+        .catch(err => console.log(err))
+    },
+    redirect: function () {
+      this.$router.push({ name: 'dashboard', params: { cadastro: true } })
     }
   }
 }
